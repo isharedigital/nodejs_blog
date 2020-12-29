ab97bbd9666256ce0104d0e8ec0c0d5b426f7af5
Sơn Nguyễn Xuân <sonnguyen.digital@gmail.com>
2020-12-29 15:16:06 +0700

    install scss and static file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const handlebars = require('express-handlebars');
 const app = express()
 const port = 3000
 
+//Clare static files
+app.use(express.static(path.join(__dirname, 'public')))
+
 //HTTPS logegr
 app.use(morgan('combined'))
 
